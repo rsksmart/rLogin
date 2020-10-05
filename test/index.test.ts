71bababfd59d64f2e224ea40b5e5fdf81040818e
Ilan Olkies <ilanolkies@outlook.com>
2020-10-05 16:30:14 -0300

    Change dummy test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,5 @@
-import rLogin from '../src'
-
 describe('rLogin default export', () => {
   test('welcome', () => {
-    expect(rLogin()).toEqual('Welcome to rLogin')
+    expect(true).toBeTruthy()
   })
 })
