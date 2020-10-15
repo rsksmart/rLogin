e3bf7279c2cf036e612bcb12d3e24d8128ce9310
Jesse Clark <hello@developerjesse.com>
2020-10-15 16:47:18 +0300

    Add testing framework (#6)
    * Setup Enzyme with test coverage.
    
    * Add Modal.test.ts, add jsdom, create mock for localStorage
    
    * Mock and import variables needed for Modal.
    
    * Update Modal.test.tsx
    
    Co-authored-by: Ilan <36084092+ilanolkies@users.noreply.github.com>

---
+++ src/components/Modal.test.tsx
@@ -0,0 +1,49 @@
+// eslint-disable-next-line
+import React from 'react'
+import { shallow } from 'enzyme'
+import { Modal } from './Modal'
+import { ThemeColors, IProviderUserOptions, ProviderController, IProviderControllerOptions, IProviderOptions } from 'web3modal'
+
+describe('Component: Modal', () => {
+  it('should render and be described', () => {
+    const themeColors: ThemeColors = {
+      background: '#FFFFFF',
+      main: '#000000',
+      secondary: '#000000',
+      border: '#FFFFFF',
+      hover: '#FFFFFF'
+    }
+
+    const option: IProviderUserOptions = {
+      name: 'test',
+      logo: 'test.jpg',
+      description: 'description',
+      onClick: jest.fn()
+    }
+    const providerOptions: IProviderOptions = {
+      null: {
+        package: 'any'
+      }
+    }
+    const providerControllerOptions: IProviderControllerOptions = {
+      disableInjectedProvider: false,
+      cacheProvider: false,
+      providerOptions: providerOptions,
+      network: 'string'
+    }
+
+    const wrapper = shallow(
+      <Modal
+        themeColors={themeColors}
+        userOptions={[option]}
+        onClose={jest.fn()}
+        resetState={jest.fn()}
+        lightboxOpacity={60}
+        providerController={new ProviderController(providerControllerOptions)}
+        onConnect={jest.fn()}
+        onError={jest.fn()}
+      />
+    )
+    expect(wrapper).toBeDefined()
+  })
+})
---
+++ test/setupTests.ts
@@ -0,0 +1,9 @@
+import 'jsdom-global/register'
+import * as Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import LocalStorageMock from './LocalStorageMock'
+
+// Setup enzyme's react adapter
+Enzyme.configure({ adapter: new Adapter() })
+
+Object.defineProperty(window, 'localStorage', { value: LocalStorageMock })
