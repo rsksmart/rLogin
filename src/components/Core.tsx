ca7e0bd85619f5e0d0e99e014a65f8b2022ba63a
Ilan <36084092+ilanolkies@users.noreply.github.com>
2020-11-13 12:00:20 -0300

    Read state property 'sdr'
    https://lgtm.com/projects/g/rsksmart/rLogin/snapshot/af5893ac13ccf07153c4fd4896512683bbce90c9/files/src/components/Core.tsx#xf6ddff8627b8a14d:1

--- src/components/Core.tsx
+++ src/components/Core.tsx
@@ -201,7 +201,7 @@ export class Core extends React.Component<IModalProps, IModalState> {
       mainModalCard={this.mainModalCard}
     >
       {currentStep === 'Step1' && <WalletProviders userProviders={userProviders} />}
-      {currentStep === 'Step2' && <p>Access to Data Vault not supported yet</p>}
+      {currentStep === 'Step2' && <p>Access to Data Vault not supported yet<br />SDR: {sdr.toString()}</p>}
       {currentStep === 'Step3' && <ConfirmSelectiveDisclosure did={(chainId && address) ? getDID(chainId, address) : ''} sd={sd} onConfirm={this.onConfirmAuth} />}
       {currentStep === 'error' && <ErrorMessage title={errorReason?.title} description={errorReason?.description}/>}
     </Modal>
