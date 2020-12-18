33c44a99a05a53a4a1f0d68f70dc8c1250a0702c
Ilan Olkies <ilanolkies@outlook.com>
2020-12-18 16:14:41 -0300

    Fix css selector

---
+++ src/constants/cssSelectors.test.ts
@@ -0,0 +1,13 @@
+import * as CSSSelector from './cssSelectors'
+
+describe('CSS Selectors', () => {
+  test('are all different', () => {
+    let valuesForKeys: { [key: string]: any } = {}
+
+    Object.keys(CSSSelector).forEach(key => {
+      const value = (CSSSelector as any)[key]
+      expect(valuesForKeys[value]).toBeUndefined()
+      valuesForKeys[value] = key
+    })
+  })
+})
