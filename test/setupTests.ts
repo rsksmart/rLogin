9be825460c33c4a8b4afa1e3b48be0f32de49e90
Jesse Clark <hello@developerjesse.com>
2021-01-28 18:46:50 +0200

    Remove duplicate mock.

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -1,7 +1,7 @@
 import 'jsdom-global/register'
 import * as Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import LocalStorageMock from './LocalStorageMock'
+import LocalStorageMock from '../src/test/LocalStorageMock'
 
 // Setup enzyme's react adapter
 Enzyme.configure({ adapter: new Adapter() })
