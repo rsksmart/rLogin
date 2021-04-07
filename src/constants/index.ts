1b80b98b2b0911958535868cb949f93d15083f1c
Jesse Clark <hello@developerjesse.com>
2021-04-07 15:55:28 +0300

    Refactor code to create new disconnectWC function that disconnects the provider and removes the walletconnect LSvariable.

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
 export const RLOGIN_REFRESH_TOKEN = 'RLOGIN_REFRESH_TOKEN'
 export const RLOGIN_ACCESS_TOKEN = 'RLOGIN_ACCESS_TOKEN'
 export const DECRYPT_ERROR = 'DECRYPT_ERROR'
+
+export const WALLETCONNECT = 'walletconnect'
