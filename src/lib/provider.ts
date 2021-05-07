82c531666a1e831295429b0717688711603f0024
Jesse Clark <hello@developerjesse.com>
2021-05-07 16:40:59 +0300

    support older providers when .request is not available.

--- src/lib/provider.ts
+++ src/lib/provider.ts
@@ -12,8 +12,22 @@ export interface EIP1193Provider {
   isNiftyWallet: boolean | null
 }
 
-export const ethAccounts = (provider: EIP1193Provider) => provider.request<string[]>({ method: 'eth_accounts' })
-export const ethChainId = (provider: EIP1193Provider) => provider.request<string>({ method: 'eth_chainId' })
+export interface OlderProvider {
+  send(param: string): Promise<any>
+}
+
+export interface GenericProvider extends EIP1193Provider, OlderProvider {}
+
+export const ethAccounts = (provider: GenericProvider) =>
+  provider.request
+    ? provider.request<string[]>({ method: 'eth_accounts' })
+    : provider.send('eth_accounts')
+
+export const ethChainId = (provider: GenericProvider) =>
+  provider.request
+    ? provider.request<string>({ method: 'eth_chainId' })
+    : provider.send('eth_chainId')
+
 export const personalSign = (provider: EIP1193Provider, address: string, data: string) => provider.request({ method: 'personal_sign', params: [data, address] })
 export const addEthereumChain = (provider: EIP1193Provider, params: AddEthereumChainParameter) => provider.request({ method: 'wallet_addEthereumChain', params: [params] })
 
