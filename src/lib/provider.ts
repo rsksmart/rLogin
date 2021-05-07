e9307c7b1ae3d992943f475d57c44fc7ff8f7442
Jesse Clark <hello@developerjesse.com>
2021-05-07 17:58:05 +0300

    Add support for 2nd flavor, signing messages.

--- src/lib/provider.ts
+++ src/lib/provider.ts
@@ -1,4 +1,5 @@
 import { AddEthereumChainParameter } from '../ux/wrongNetwork/changeNetwork'
+import { utf8ToHex } from 'web3-utils'
 
 // https://eips.ethereum.org/EIPS/eip-1193
 interface RequestArguments {
@@ -13,7 +14,8 @@ export interface EIP1193Provider {
 }
 
 export interface OlderProvider {
-  send(param: string): Promise<any>
+  send(param: string | { method: string, params: string[]}, callback?: ((err: Error, res: any) => void) | undefined): Promise<any>
+  isPortis: boolean | null
 }
 
 export interface GenericProvider extends EIP1193Provider, OlderProvider {}
@@ -28,7 +30,15 @@ export const ethChainId = (provider: GenericProvider) =>
     ? provider.request<string>({ method: 'eth_chainId' })
     : provider.send('eth_chainId')
 
-export const personalSign = (provider: EIP1193Provider, address: string, data: string) => provider.request({ method: 'personal_sign', params: [data, address] })
+export const personalSign = (provider: GenericProvider, address: string, data: string) => {
+  return (provider.isPortis)
+    ? new Promise((resolve, reject) => provider.send(
+      { method: 'personal_sign', params: [utf8ToHex(data), address] },
+      (err: Error, res: { result: string }) => err ? reject(err) : resolve(res.result)
+    ))
+    : provider.request({ method: 'personal_sign', params: [data, address] })
+}
+
 export const addEthereumChain = (provider: EIP1193Provider, params: AddEthereumChainParameter) => provider.request({ method: 'wallet_addEthereumChain', params: [params] })
 
 export const isMetamask = (provider: EIP1193Provider) => provider.isMetaMask && !provider.isNiftyWallet
