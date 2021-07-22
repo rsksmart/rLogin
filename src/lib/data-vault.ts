55c9b29c93ef3d8275a767a99fb5b41e204165da
Diego Grasso <diegograsso@iovlab.org>
2021-07-22 10:14:48 -0300

    Applying more types

--- src/lib/data-vault.ts
+++ src/lib/data-vault.ts
@@ -1,7 +1,8 @@
 import { EIP1193Provider, isMetamask } from './provider'
+import { IIPFSCpinnerClient as DataVault } from '@rsksmart/ipfs-cpinner-client-types'
 
 export const createDataVault = (serviceUrl:String, dataVaultClient: any) => {
-  return async (provider: EIP1193Provider, did: string, address: string) => {
+  return async (provider: EIP1193Provider, did: string, address: string): Promise<DataVault> => {
     const personalSign = (data: string) => provider.request({ method: 'personal_sign', params: [data, address] })
 
     // eslint-disable-next-line new-cap
@@ -11,9 +12,9 @@ export const createDataVault = (serviceUrl:String, dataVaultClient: any) => {
       encryptionManager: isMetamask(provider)
         ? await dataVaultClient.AsymmetricEncryptionManager.fromWeb3Provider(provider)
         : await dataVaultClient.SignerEncryptionManager.fromWeb3Provider(provider)
-    })
+    }) as DataVault
   }
 }
 
-export const getContentsFromDataVault = (dataVault: any, did: string, key: string) => dataVault!.get({ key })
-  .then((contents: { content: any }[]) => contents.map(({ content }) => content))
+export const getContentsFromDataVault = (dataVault: DataVault, did: string, key: string) => dataVault!.get({ key })
+  .then((contents) => contents.map(({ content }) => content))
