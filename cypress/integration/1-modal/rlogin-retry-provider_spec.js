cd9390990d0367c3e388edfaa4c8c18b99e88d2b
diegograsso <diego.grasso@gmail.com>
2021-08-10 09:38:40 -0300

    US-60- Fix reconnecting after a failed connect issue (#120)
    * Fix
    
    * Added test for disconnect clean up after error
    
    Co-authored-by: Diego Grasso <diegograsso@iovlab.org>

---
+++ cypress/integration/1-modal/rlogin-retry-provider_spec.js
@@ -0,0 +1,37 @@
+import currentProvider from '@rsksmart/mock-web3-provider'
+
+describe('modal - chose metamask fails, then walletconnect works', () => {
+  const address = '0xB98bD7C7f656290071E52D1aA617D9cB4467Fd6D';
+  const privateKey = 'de926db3012af759b4f24b5a51ef6afa397f04670f634aa4f48d4480417007f3'
+
+  beforeEach(() => {
+    cy.on('window:before:load', (win) => {
+      win.ethereum = currentProvider({
+        address,
+        privateKey,
+        chainId: 31,
+        debug: true
+      })
+      win.ethereum.request = () => Promise.reject(new Error('an error'))
+    })
+  })
+
+  it('Try to open Metamask but fails', () => { // no error here until web3modal fixes the error return for connect
+    cy.visit('/')
+    cy.contains('login with rLogin').click()
+    cy.contains('MetaMask').click()
+    cy.get('.rlogin-modal-lightbox').should('be.not.visible')
+  })
+
+  it('retry to open Metamask but fails', () => { // no error here until web3modal fixes the error return for connect
+    cy.contains('login with rLogin').click()
+    cy.contains('MetaMask').click()
+    cy.get('.rlogin-modal-lightbox').should('be.not.visible')
+  })
+
+  it('shows a QR code for WalletConnect', () => { // try with WalletConnect should work
+    cy.contains('login with rLogin').click()
+    cy.contains('WalletConnect').click()
+    cy.get('#walletconnect-qrcode-text').should('exist')
+  })
+})
