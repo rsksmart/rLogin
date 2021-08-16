1ebdbe737b46653595abdf19e1c3ac92136cdb3d
Luis Chavarría <luis.chavarria@iovlabs.org>
2021-08-16 10:11:19 -0600

    feat: detect default language from browser

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,4 +1,6 @@
 import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
 import { initReactI18next } from 'react-i18next'
 
 // the translations
@@ -51,12 +53,9 @@ const resources = {
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
   .init({
     resources,
-    lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
-
     interpolation: {
       escapeValue: false // react already safes from xss
     }
