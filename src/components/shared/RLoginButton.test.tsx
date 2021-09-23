5be23b8f7cef56d8192ca26dd3ac842c69574260
Jesse Clark <hello@developerjesse.com>
2021-09-23 15:39:11 +0300

    Remove styled components and do it with css.

--- src/components/shared/RLoginButton.test.tsx
+++ src/components/shared/RLoginButton.test.tsx
@@ -3,9 +3,23 @@ import { mount } from 'enzyme'
 import { RLoginButton } from './RLoginButton'
 
 describe('Component: RLoginButton', () => {
-  const wrapper = mount(<RLoginButton onClick={jest.fn()}>Hello World!</RLoginButton>)
-  it('renders the component', () => {
+  it('renders the component with text', () => {
+    const wrapper = mount(<RLoginButton onClick={jest.fn()}>Hello World!</RLoginButton>)
     expect(wrapper).toBeDefined()
     expect(wrapper.text()).toBe('Hello World!')
+  })
+
+  it('handles click method', () => {
+    const onClick = jest.fn()
+    const wrapper = mount(<RLoginButton onClick={onClick}>Hello World!</RLoginButton>)
+    wrapper.find('button').simulate('click')
+    expect(onClick).toBeCalledTimes(1)
+  })
+
+  it('does not click when it is disabled', () => {
+    const onClick = jest.fn()
+    const wrapper = mount(<RLoginButton disabled={true} onClick={onClick}>Hello World!</RLoginButton>)
+    wrapper.find('button').simulate('click')
+    expect(onClick).toBeCalledTimes(0)
   })
 })
--- src/components/shared/RLoginButton.tsx
+++ src/components/shared/RLoginButton.tsx
@@ -1,31 +1,34 @@
 import React, { ReactNode } from 'react'
-import styled from 'styled-components'
+
 interface RLoginButtonInterface {
   children: ReactNode
   onClick?: () => void
   disabled?: boolean
 }
 
-const ButtonWrapper = styled.button<{ disabled: boolean }>`
-  display: 'inline-block';
-  margin: '10px';
-  padding: '10px 50px';
-  border: 'none';
-  border-radius: '5px';
-  box-shadow: '0px 4px 10px rgba(0,0,0,0.1)';
-  font-size: '18px';
-  line-height: '100%';
-  cursor: ${(disabled) => disabled ? 'auto' : 'pointer'};
-  backgroundColor: '#008FF7';
-  color: '#FFFFFF';
-`
-
-export const RLoginButton: React.FC<RLoginButtonInterface> = ({ children, disabled, onClick }) => {
-  return (
-    <ButtonWrapper
-      onClick={onClick}
-      className="rlogin-button"
-      disabled={disabled || false}
-    >{children}</ButtonWrapper>
-  )
+const buttonStyles = {
+  display: 'inline-block',
+  margin: '10px',
+  padding: '10px 50px',
+  border: 'none',
+  borderRadius: '5px',
+  boxShadow: '0px 4px 10px rgba(0,0,0,0.1)',
+  fontSize: '18px',
+  lineHeight: '100%',
+  backgroundColor: '#008FF7',
+  color: '#FFF',
+  cursor: 'pointer'
 }
+
+export const RLoginButton: React.FC<RLoginButtonInterface> = ({ children, disabled, onClick }) => (
+  <button
+    onClick={onClick}
+    className="rlogin-button"
+    disabled={disabled || false}
+    style={{
+      ...buttonStyles,
+      cursor: disabled ? 'auto' : 'pointer',
+      opacity: disabled ? 0.5 : 1
+    }}
+  >{children}</button>
+)
