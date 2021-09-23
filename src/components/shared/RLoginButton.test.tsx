ffd7e25df2cc501140c3546910d2a0a233c7722d
Jesse Clark <hello@developerjesse.com>
2021-09-23 14:56:08 +0300

    Fix error.

--- src/components/shared/RLoginButton.test.tsx
+++ src/components/shared/RLoginButton.test.tsx
@@ -3,7 +3,7 @@ import { mount } from 'enzyme'
 import { RLoginButton } from './RLoginButton'
 
 describe('Component: RLoginButton', () => {
-  const wrapper = mount(<RLoginButton onclick={jest.fn()}>Hello World!</RLoginButton>)
+  const wrapper = mount(<RLoginButton onClick={jest.fn()}>Hello World!</RLoginButton>)
   it('renders the component', () => {
     expect(wrapper).toBeDefined()
     expect(wrapper.text()).toBe('Hello World!')
