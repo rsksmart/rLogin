7896560186c214adb58d256a84f087e163e60eca
Ilan <ilanolkies@outlook.com>
2021-09-28 19:32:17 -0300

    Lint

--- src/controllers/providers.ts
+++ src/controllers/providers.ts
@@ -1,4 +1,4 @@
-import { NetworkParams } from '../lib/networkOptionsTypes';
+import { NetworkParams } from '../lib/networkOptionsTypes'
 import { EventController, IProviderInfo, IProviderDisplayWithConnector, IProviderOptions, IProviderControllerOptions, getLocal, CACHED_PROVIDER_KEY, getInjectedProvider, INJECTED_PROVIDER_ID, getProviderInfoById, findMatchingRequiredOptions, isMobile, getProviderDescription, filterMatches, removeLocal, setLocal, CONNECT_EVENT, connectors, injected, providers } from 'web3modal'
 
 import { RLoginIProviderUserOptions } from '../Core'
