95a74040d29342e2c41e6b1b015e68ae0c10af3c
Christian Escalante <chescalante@gmail.com>
2021-10-28 20:29:32 -0300

    fix tests

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -1,7 +1,39 @@
 import 'jsdom-global/register'
 import * as Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import LocalStorageMock from './localStorageMock'
+
+interface IStore {
+    [key: string]: any
+  }
+
+class LocalStorageMock {
+    private store: IStore
+
+    constructor () {
+      this.store = {}
+
+      this.getItem.bind(this)
+      this.setItem.bind(this)
+      this.clear.bind(this)
+      this.removeItem.bind(this)
+    }
+
+    clear () {
+      this.store = {}
+    }
+
+    getItem (key: string) {
+      return this.store[key] || null
+    }
+
+    setItem (key: string, value: any) {
+      this.store[key] = String(value)
+    }
+
+    removeItem (key: string) {
+      delete this.store[key]
+    }
+};
 
 // Setup enzyme's react adapter
 Enzyme.configure({ adapter: new Adapter() })
