3f8778fef3e5bcaf460cc86ee867ef88d6582646
Jesse Clark <hello@developerjesse.com>
2021-11-12 17:52:36 +0200

    Add test to show hardware flow.

--- cypress/integration/2-sample-app/cacheProvider_spec.js
+++ cypress/integration/2-sample-app/cacheProvider_spec.js
@@ -1,13 +1,15 @@
 import currentProvider from '@rsksmart/mock-web3-provider'
 
-describe('sample:dapp testing, no backend', () => {
+describe('cache provider tests', () => {
   const address = '0xB98bD7C7f656290071E52D1aA617D9cB4467Fd6D'
   const privateKey = 'de926db3012af759b4f24b5a51ef6afa397f04670f634aa4f48d4480417007f3'
 
   const rLoginCached = JSON.stringify({
-    "name": "MetaMask",
-    "logo": "data:image/svg+xml;",
-    "description": "Connect to your MetaMask Wallet"
+    provider: {
+      "name": "MetaMask",
+      "logo": "data:image/svg+xml;",
+      "description": "Connect to your MetaMask Wallet"
+    }
   })
 
   beforeEach(() => {
@@ -84,6 +86,18 @@ describe('sample:dapp testing, no backend', () => {
   })
 
   describe('sample:permissioned', () => {
+    it('logs in with injected', () => {
+      cy.visit('/?cache=yes&backend=yes', {
+        onBeforeLoad: function (window) {
+          window.localStorage.setItem('WEB3_CONNECT_CACHED_PROVIDER', '"injected"')
+          window.localStorage.setItem('RLOGIN_SELECTED_PROVIDER', rLoginCached)
+        }
+      })
+
+      cy.get('#login').click()
+      cy.get('.rlogin-header2').should('have.text', 'Would you like to give us access to info in your data vault?')
+    })
+
     it('resets with a junk provider', () => {
       cy.visit('/?cache=yes&backend=yes', {
         onBeforeLoad: function (window) {
