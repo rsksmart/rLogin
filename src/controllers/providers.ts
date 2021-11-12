5cbb7c789fa01461792d5a2eaf2f51bf07617205
Jesse Clark <hello@developerjesse.com>
2021-11-12 15:55:17 +0200

    Refactor connectToCacheProvider to return a proper promise.

--- src/controllers/providers.ts
+++ src/controllers/providers.ts
@@ -208,11 +208,13 @@ export class RLoginProviderController {
     })
   };
 
-  public async connectToCachedProvider () {
-    const provider = this.getProvider(this.cachedProvider)
-    if (typeof provider !== 'undefined') {
-      await this.connectTo(provider.id, provider.connector)
-    }
+  public connectToCachedProvider () {
+    return new Promise((resolve, reject) => {
+      const provider = this.getProvider(this.cachedProvider)
+      typeof provider !== 'undefined'
+        ? resolve(this.connectTo(provider.id, provider.connector))
+        : reject(new Error('Provider not found'))
+    })
   }
 
   public on (event: string, callback: (result: any) => void): () => void {
