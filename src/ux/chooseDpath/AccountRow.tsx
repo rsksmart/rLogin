6dc62c8f45bf95abc6d77e5de760e0b4c272de47
Jesse Clark <hello@developerjesse.com>
2022-01-18 14:36:03 +0200

    Show balances.

--- src/ux/chooseDpath/AccountRow.tsx
+++ src/ux/chooseDpath/AccountRow.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import BN from 'bn.js'
 import { shortAddress } from '../confirmInformation/ConfirmInformation'
 import { AccountInterface } from './ChooseDPath'
 
@@ -10,10 +11,12 @@ interface Interface {
 }
 
 const DpathRowStyles = styled.button<{ selected: boolean }>`
+  display: flex;
   background: ${(props) => props.selected ? props.theme.overlay : props.theme.primaryText};
   border: none;
   border-radius: 5px;
   padding: 5px;
+  margin-bottom: 5px;
   font-weight: 400 !important;
   font-size: 12px;
   color: ${(props) => props.selected ? props.theme.primaryText : props.theme.p};
@@ -21,13 +24,25 @@ const DpathRowStyles = styled.button<{ selected: boolean }>`
   cursor: pointer
 `
 
-const AccountRow: React.FC<Interface> = ({ account, selected, onClick }: Interface) =>
-  <DpathRowStyles onClick={onClick} selected={selected}>
-    {account.dPath}
-    :
-    {shortAddress(account.address)}
-    :
-    {account.balance}
+const Column = styled.div`
+  width: 33%;
+  text-align: left;
+  padding: 0 5px;
+`
+
+const AccountRow: React.FC<Interface> = ({ account, selected, onClick }: Interface) => {
+  const balance = account.balance && account.balance.startsWith('0x')
+    ? new BN(account.balance.substring(2), 16)
+    : new BN(account.balance || 0, 16)
+
+  // BN.js does not handle decimals
+  const niceBalance = balance.toNumber() / 1000000000000000000
+
+  return <DpathRowStyles onClick={onClick} selected={selected}>
+    <Column>{account.dPath}</Column>
+    <Column>{shortAddress(account.address)}</Column>
+    <Column>{niceBalance.toString().substring(0, 10)} RBTC</Column>
   </DpathRowStyles>
+}
 
 export default AccountRow
--- src/ux/chooseDpath/ChooseDPath.tsx
+++ src/ux/chooseDpath/ChooseDPath.tsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { getDPathByChainId } from '@rsksmart/rlogin-dpath'
 import { isLedger } from '../../lib/hardware-wallets'
-import { WideBox } from '../../ui/shared/Box'
-import { Paragraph } from '../../ui/shared/Typography'
+import { Header2, Header3, Paragraph } from '../../ui/shared/Typography'
 import LoadingComponent from '../../ui/shared/Loading'
 import { Button } from '../../ui/shared/Button'
 import AccountRow from './AccountRow'
@@ -23,6 +22,20 @@ const DPathInput = styled.input`
   &:focus {
     outline: none;
 }
+`
+
+const Row = styled.div`
+  display: flex;
+  font-size: 14px;
+  color: ${props => props.theme.h3};
+  border-bottom: ${(props) => `1px solid ${props.theme.p}`};
+  padding-bottom: 5px;
+`
+
+export const Column = styled.div`
+  width: 33%;
+  text-align: left;
+  padding: 0 5px 0 10px;
 `
 
 interface Interface {
@@ -45,7 +58,10 @@ export const ChooseDPathComponent: React.FC<Interface> = ({
     provider.getAddresses(accountIds)
       .then((accounts: AccountInterface[]) => {
         const balanceRequests = accountIds.map((id) =>
-          provider.request({ method: 'eth_getBalance', params: [accounts[id].address, 'latest'] }))
+          provider.request({
+            method: 'eth_getBalance',
+            params: [accounts[id].address.toLowerCase(), 'latest']
+          }))
 
         // get the balances
         Promise.all(balanceRequests)
@@ -54,8 +70,7 @@ export const ChooseDPathComponent: React.FC<Interface> = ({
               // add balances to the account objects
               accountIds.map((id: number) => (
                 { ...accounts[id], balance: balances[id] }
-              )))
-          )
+              ))))
       })
       .catch(handleError)
   }, [provider])
@@ -65,22 +80,17 @@ export const ChooseDPathComponent: React.FC<Interface> = ({
     .catch(handleError)
 
   if (allAccounts.length === 0) {
-    return <LoadingComponent text="retrieving addresses" />
+    return <LoadingComponent text="retrieving addresses and balances" />
   }
 
-  return <WideBox>
-    <Paragraph>Using:
-      <DPathInput
-        type="text"
-        className="final-dpath"
-        value={selectedAccount}
-        onChange={e => setSelectedAccount(e.target.value)}
-      />
-    </Paragraph>
+  return <>
+    <Header2>Select an account</Header2>
 
-    <Paragraph>
-      Standard base derivation path: {provider.path}
-    </Paragraph>
+    <Row>
+      <Column>Path</Column>
+      <Column>Address</Column>
+      <Column>Balance</Column>
+    </Row>
 
     {allAccounts.map((account: AccountInterface) =>
       <AccountRow
@@ -90,8 +100,19 @@ export const ChooseDPathComponent: React.FC<Interface> = ({
         onClick={() => setSelectedAccount(account.dPath)}
       />
     )}
+
+    <Header3>Or choose a specific path</Header3>
+    <Paragraph>Current Path:
+      <DPathInput
+        type="text"
+        className="final-dpath"
+        value={selectedAccount}
+        onChange={e => setSelectedAccount(e.target.value)}
+      />
+    </Paragraph>
+
     <Button onClick={handleSelectAccount}>Select</Button>
-  </WideBox>
+  </>
 }
 
 export const initialDPath = (selectedChainId: string, providerName: string) => getDPathByChainId(parseInt(selectedChainId), 0, isLedger(providerName)).slice(2)
