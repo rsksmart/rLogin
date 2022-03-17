be376c4094114938ac8cbf0ba24fae03b3c675e8
Luis Chavarría <luis.chavarria@iovlabs.org>
2022-03-17 16:25:59 -0600

    fix: change copy

--- src/i18n.ts
+++ src/i18n.ts
@@ -68,7 +68,7 @@ const resources = {
       Balance: 'Balance',
       'Or use the textbox to choose a specific path:': 'Or use the textbox to choose a specific path:',
       'Use Ethereum path (check this if you used to connect with Metamask)': 'Use Ethereum path (check this if you used to connect with Metamask)',
-      'Please open your wallet': 'Please open your wallet'
+      'Please confirm in your wallet': 'Please confirm in your wallet'
     }
   },
   es: {
@@ -100,7 +100,7 @@ const resources = {
       Network: 'Red',
       'Do not show again': 'No mostrar de nuevo',
       'Connected wallet': 'Billetera conectada',
-      'Please open your wallet': 'Por favor revisa tu wallet'
+      'Please confirm in your wallet': 'Por favor confirmar en su wallet'
     }
   },
   ja: {
