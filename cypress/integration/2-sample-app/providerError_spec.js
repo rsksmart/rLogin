a98bbc6c61b49fd20becfd33d3280fe3abd93a5e
Ilan <ilanolkies@outlook.com>
2022-05-27 00:40:02 -0300

    Refactor account of integration tests

--- cypress/integration/2-sample-app/providerError_spec.js
+++ cypress/integration/2-sample-app/providerError_spec.js
@@ -1,9 +1,7 @@
 import { MockProvider } from '@rsksmart/mock-web3-provider'
+import { privateKey, address } from '../account'
 
 describe('Web3 Provider throws an error when connecting', () => {
-  const address = '0xB98bD7C7f656290071E52D1aA617D9cB4467Fd6D'
-  const privateKey = 'de926db3012af759b4f24b5a51ef6afa397f04670f634aa4f48d4480417007f3'
-
   beforeEach(() => {
     const provider = new MockProvider({
       address,
