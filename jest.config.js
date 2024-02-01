4f033d0cced433674cfb9de7e0c6412364bb9d11
Francis Rodriguez <francisngg@gmail.com>
2024-02-01 14:05:44 -0400

    package.json removed duplicated jest config. jest.config.js added axios as in the newest version it changed to ECMAScript.

--- jest.config.js
+++ jest.config.js
@@ -80,7 +80,7 @@ module.exports = {
   ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  moduleNameMapper: { 'aurelia-(.*)': '<rootDir>/node_modules/$1' },
+  moduleNameMapper: { 'aurelia-(.*)': '<rootDir>/node_modules/$1', '^axios$': 'axios/dist/node/axios.cjs' },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
